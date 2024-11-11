bac6ab216b40acb278ce32468d78ad0668ba7243
JohanDumouchel <johandumouchel@gmail.com>
2024-11-11 23:31:22 +0100

    chapter 3 of expo tuto

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,38 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons'
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+    screenOptions={{
+      tabBarActiveTintColor: '#ffd33d',
+      headerStyle: {
+        backgroundColor: '#25292e',
+      },
+      headerShadowVisible: false,
+      headerTintColor: '#fff',
+      tabBarStyle: {
+        backgroundColor: '#25292e',
+      },
+    }}>
+      <Tabs.Screen name="index" options={{
+        headerTitle: "Sticker Smash",
+        tabBarIcon: ({focused,color}) => (
+          <Ionicons 
+            name={focused ? "home-sharp" : "home-outline"} 
+            color={color}
+            size={24} />
+        ),
+      }} />
+      <Tabs.Screen name="about" options={{
+        headerTitle:"About",
+        tabBarIcon: ({focused,color}) => (
+          <Ionicons 
+            name={focused ? "information-sharp": "information-outline"} 
+            color={color}
+            size={24} />
+        ),
+        }} />
     </Tabs>
   );
 }
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,28 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
+    <View style={styles.container}>
+      <Stack.Screen options={{title: 'Oopsy... Not found'}}/>
+      <Link href={"/"} style={styles.button}> Go back to Home screen</Link>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor:'#25292e',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  button: {
+    fontSize: 20,
+    textDecorationLine:'underline',
+    color: '#fff',
+  },
+  text: {
+    color: '#fff',
   },
 });
